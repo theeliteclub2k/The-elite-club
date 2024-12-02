41ef68a3222a5865e2666bb2b7b8ac4990c6aa56
theeliteclub2k <vanjregautham@gmail.com>
2024-12-02 11:29:23 +0530

    feat: appwrite setup

---
+++ appwrite.config.ts
@@ -0,0 +1,15 @@
+import * as sdk from "node-appwrite";
+export const {
+  PROJECT_ID,
+  AUTH_API_KEY,
+  NEXT_PUBLIC_PRODUCT_IMAGES_BUCKET_ID: BUCKET_ID,
+  NEXT_PUBLIC_ENDOINT: ENDPOINT,
+} = process.env;
+
+const client = new sdk.Client();
+
+client.setEndpoint(ENDPOINT!).setProject(PROJECT_ID!).setKey(AUTH_API_KEY!);
+
+export const storage = new sdk.Storage(client);
+export const users = new sdk.Users(client);
+export const messaging = new sdk.Messaging(client);
